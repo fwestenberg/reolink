8747393f511525684fc1b325039ec0d96388f8f1
fwestenberg <47930023+fwestenberg@users.noreply.github.com>
2020-11-13 15:03:13 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'reolink',
   packages = ['reolink'],
-  version = '0.1.0',
+  version = '0.0.5',
   license='MIT',
   description = 'Reolink camera package',
   author = 'fwestenberg',
   author_email = '',
   url = 'https://github.com/fwestenberg/reolink',
-  download_url = 'https://github.com/fwestenberg/reolink/archive/v_010.tar.gz',
+  download_url = 'https://github.com/fwestenberg/reolink/archive/v_005.tar.gz',
   keywords = ['Reolink', 'Home-Assistant'],
   install_requires=[
           'ffmpeg',
