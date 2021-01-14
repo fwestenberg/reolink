d43194af865f8ac04e695c1f5a9e438462a413e9
fwestenberg <47930023+fwestenberg@users.noreply.github.com>
2021-01-14 13:06:00 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'reolink',
   packages = ['reolink'],
-  version = '0.0.14',
+  version = '0.0.15',
   license='MIT',
   description = 'Reolink camera package',
   author = 'fwestenberg',
