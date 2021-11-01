bbbe1ddb78b1c1e7643fb711ca7fc1b8467b1740
Christophe Painchaud <shellescape@gmail.com>
2021-11-01 09:22:43 +0100

    fix for GetRec SetRec

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='reolink',
     packages=['reolink'],
-    version='0.0.39',
+    version='0.0.40',
     license='MIT',
     description='Reolink camera package',
     author='fwestenberg',
