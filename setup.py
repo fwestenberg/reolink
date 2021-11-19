10882d8f47f7dff568fa1428037681b00b7eab13
Peter T <p.tyers@bigpond.com>
2021-11-19 11:54:15 +1100

    Check to ignore cmd GetPush when model is Reolink Duo

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from distutils.core import setup
 
 setup(
@@ -28,3 +29,35 @@ setup(
         'Programming Language :: Python :: 3.9'
     ],
 )
+=======
+from distutils.core import setup
+
+setup(
+    name='reolink',
+    packages=['reolink'],
+    version='0.0.40',
+    license='MIT',
+    description='Reolink camera package',
+    author='fwestenberg',
+    author_email='',
+    url='https://github.com/fwestenberg/reolink',
+    download_url='https://github.com/fwestenberg/reolink/releases/latest',
+    keywords=['Reolink', 'Home-Assistant'],
+    install_requires=[
+        'ffmpeg',
+        'requests',
+        'aiohttp'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
+    ],
+)
+>>>>>>> d574948 (Check to ignore cmd GetPush when model is Reolink Duo)
