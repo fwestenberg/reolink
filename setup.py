925d4b671c07ecfb0610cfddbfa0891e310e3d40
Christophe Painchaud <shellescape@gmail.com>
2021-11-26 15:37:21 +0100

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='reolink',
     packages=['reolink'],
-    version='0.0.47',
+    version='0.0.48',
     license='MIT',
     description='Reolink camera package',
     author='fwestenberg',
