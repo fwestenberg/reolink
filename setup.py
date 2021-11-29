b8f5cfa8cb8f811afaaee3fd732a8cdfd15e8764
Christophe Painchaud <shellescape@gmail.com>
2021-11-29 10:57:20 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='reolink',
     packages=['reolink'],
-    version='0.0.48',
+    version='0.0.49',
     license='MIT',
     description='Reolink camera package',
     author='fwestenberg',
